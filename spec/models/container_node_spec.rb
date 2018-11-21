b6a8b4dc44e4fb40da1caeef2ad2c81096de30a2
Adam Grare <agrare@redhat.com>
2018-11-21 11:32:19 -0500

    Add ContainerNode#lives_on

--- spec/models/container_node_spec.rb
+++ spec/models/container_node_spec.rb
@@ -62,6 +62,12 @@ describe 'ContainerNode' do
     end
   end
 
+  describe 'test attribute "lives_on"' do
+    it 'should work' do
+       # assertion here. ref: https://www.relishapp.com/rspec/rspec-expectations/docs/built-in-matchers
+    end
+  end
+
   describe 'test attribute "source_created_at"' do
     it 'should work' do
        # assertion here. ref: https://www.relishapp.com/rspec/rspec-expectations/docs/built-in-matchers
